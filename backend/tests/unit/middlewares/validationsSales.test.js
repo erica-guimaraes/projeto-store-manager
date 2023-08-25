fe20b30a1a29313ec0101c3bae4edc3a222fa377
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-08-25 01:38:23 -0300

    Mais testes

--- backend/tests/unit/middlewares/validationsSales.test.js
+++ backend/tests/unit/middlewares/validationsSales.test.js
@@ -8,6 +8,18 @@ const { expect } = chai;
 chai.use(sinonChai);
 
 describe('Testes para as Middlewares de Sales', function () {
+  it('Validando a inclusão de uma venda - se existe a chave productId', async function () {
+    const next = sinon.stub().returns();
+    const req = {
+      body: [{ quantity: 1 }, { productId: 2, quantity: 5 }],
+    };
+    const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
+    await validationSales.validateProductId(req, res, next);
+
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith({ message: '"productId" is required' });
+  });
+
   it('Validando a inclusão de uma venda - se existe a quantidade', async function () {
     const next = sinon.stub().returns();
     const req = {
@@ -25,17 +37,17 @@ describe('Testes para as Middlewares de Sales', function () {
     expect(res.json.calledWith({ message: '"quantity" is required' })).to.be.equal(true);
   });
 
-  // it('Validando a inclusão de uma venda - se a quantidade é maior ou igual à 1', async function () {
-  //   const next = sinon.stub().returns();
-  //   const req = {
-  //     body: [{ productId: 1, quantity: 0 }, { productId: 2, quantity: 0 }],
-  //   };
-  //   const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
-  //   await validationSales.validateQuantity(req, res, next);
+  it('Testando middleware validateQuantity caso tenha a chave quantity mas não tenha o tamanho mínimo', async function () {
+    const next = sinon.stub().returns();
+    const req = {
+      body: [{ productId: 1, quantity: 0 }, { productId: 2, quantity: 5 }],
+    };
+    const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
+    await validationSales.validateQuantity(req, res, next);
 
-  //   expect(res.status).to.have.been.calledWith(422);
-  //   expect(res.json).to.have.been.calledWith({ message: '"quantity" must be greater than or equal to 1' });
-  // });
+    expect(res.status).to.have.been.calledWith(422);
+    expect(res.json).to.have.been.calledWith({ message: '"quantity" must be greater than or equal to 1' });
+  });
 
   it('Testando Next', async function () {
     const next = sinon.stub().returns();
